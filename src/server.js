7e4e109e1f737ccc9a88ded3e532d94a0b04be4b
Miyazmm <alinakulichenko1@gmail.com>
2026-09-06 09:48:58 +0300

    tasks completed -- routes /notes, /notes/:noteID, /test-error, + error-handling middlewares added

--- src/server.js
+++ src/server.js
@@ -1,16 +1,68 @@
 // src/server.js
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
+import pino from 'pino-http';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
-// Перший маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// ROUTES
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
 });
 
-// Запуск сервера
+app.get('/notes/:noteID', (req, res) => {
+  const id_param = Number(req.params.noteID);
+  res.status(200).json(`Retrieved note with ID: ${id_param}`);
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// MIDDLEWARES
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === "production";
+
+  res.status(500).json({
+    message: isProd
+      ? "Something went wrong. Please try again later."
+      : err.message,
+  });
+});
+
+// RUN
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

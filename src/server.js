2ea4ec7c60173da76300a9f16876aad30b7d5366
Miyazmm <alinakulichenko1@gmail.com>
2026-09-06 10:26:12 +0300

    ID changed to Id; return message turned into a message

--- src/server.js
+++ src/server.js
@@ -33,9 +33,11 @@ app.get('/notes', (req, res) => {
   });
 });
 
-app.get('/notes/:noteID', (req, res) => {
-  const id_param = req.params.noteID;
-  res.status(200).json(`Retrieved note with ID: ${id_param}`);
+app.get('/notes/:noteId', (req, res) => {
+  const id_param = req.params.noteId;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${id_param}`
+  });
 });
 
 app.get('/test-error', () => {

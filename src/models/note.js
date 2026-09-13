69d7a4248e3988c4da3e0cace3e35af508ea404e
Miyazmm <alinakulichenko1@gmail.com>
2026-09-13 14:49:00 +0300

    students testing removed + notes routes adjusted

---
+++ src/models/note.js
@@ -0,0 +1,28 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true, // прибирає пробіли на початку та в кінці
+    },
+    content: {
+      type: String,
+      default: false,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
+import notesRoutes from './routes/notesRoutes.js';
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
@@ -19,18 +21,7 @@ app.use(cors());
 
 // ROUTES
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const id_param = req.params.noteId;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${id_param}`
-  });
-});
+app.use(notesRoutes);
 
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
